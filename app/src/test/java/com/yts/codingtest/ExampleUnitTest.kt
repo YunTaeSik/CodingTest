5d1fd293c868ee6153bd74a1b4c6e3555ba9fb97
yts <sky877kr@gmail.com>
2020-01-22 17:11:41 +0900

    1. 모의고사 완전탐색 풀이 완료

--- app/src/test/java/com/yts/codingtest/ExampleUnitTest.kt
+++ app/src/test/java/com/yts/codingtest/ExampleUnitTest.kt
@@ -13,11 +13,48 @@ import java.lang.StringBuilder
 class ExampleUnitTest {
     @Test
     fun test() {
-        solution("aabbaccc")
+        // stringCompression("aabbaccc")
+        var answer = fullSearch(intArrayOf(1, 2, 3, 4, 5))
+    }
+
+    /**모의고사 완전탐색**/
+    fun fullSearch(answers: IntArray): IntArray {
+        var answer = arrayListOf<Int>()
+
+        var onePerson = intArrayOf(1, 2, 3, 4, 5)
+        var twoPerson = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
+        var threePerson = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+
+        var onePersonAnswer = 0
+        var twoPersonAnswer = 0
+        var threePersonAnswer = 0
+
+        for (i in answers.indices) {
+            var example = answers[i]
+            if (example == onePerson[i % onePerson.size]) {
+                onePersonAnswer++
+            }
+            if (example == twoPerson[i % twoPerson.size]) {
+                twoPersonAnswer++
+            }
+            if (example == threePerson[i % threePerson.size]) {
+                threePersonAnswer++
+            }
+        }
+
+        var answerCounts = intArrayOf(onePersonAnswer, twoPersonAnswer, threePersonAnswer)
+
+        var max = answerCounts.max()
+        for (i in answerCounts.indices) {
+            if (max == answerCounts[i]) {
+                answer.add(i + 1)
+            }
+        }
+        return answer.toIntArray()
     }
 
     /**문자열 압축**/
-    fun solution(s: String): Int {
+    fun stringCompression(s: String): Int {
         var answer = Int.MAX_VALUE
 
         var length = s.length
--- app/src/test/java/com/yts/codingtest/StudyUnitTest.kt
+++ app/src/test/java/com/yts/codingtest/StudyUnitTest.kt
@@ -5,25 +5,28 @@ import org.junit.Test
 class StudyUnitTest {
     @Test
     fun result() {
+       var values : IntArray = intArrayOf(1,2,3,4,5)
 
+        println("max = ${max(values)}")
+        println("min = ${min(values)}")
     }
 
-    fun max(values: IntArray) : Int{
+    fun max(values: IntArray): Int {
         var answer = Int.MIN_VALUE
 
-        for(value in values){
-            if(value > answer){
+        for (value in values) {
+            if (value > answer) {
                 answer = value
             }
         }
         return answer
     }
 
-    fun min(values: IntArray) : Int{
+    fun min(values: IntArray): Int {
         var answer = Int.MAX_VALUE
 
-        for(value in values){
-            if(value < answer){
+        for (value in values) {
+            if (value < answer) {
                 answer = value
             }
         }
